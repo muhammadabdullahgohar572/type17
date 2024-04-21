c65ce988446d5e0d5decf3daa201863a6ddf57b5
Muhammad Abdullah Gohar <148785694+muhammadabdullahgohar572@users.noreply.github.com>
2024-04-21 05:47:26 -0700

    Update Test.ts

--- Test.ts
+++ Test.ts
@@ -197,37 +197,222 @@
 
 // console.log(perso1.intro());
 
-class Person {
-  name: string;
-  age: number;
-  hobbies: string[];
+/////////////[]
 
-  constructor(name: string, age: number, hobbies: string[]) {
-    this.name = name;
-    this.age = age;
-    this.hobbies = hobbies;
-  }
-  Test(): string {
-    return `This is a name ${this.name} and THIS is a age ${
-      this.age
-    } and my hobbies is ${this.hobbies.join(",")} `;
-  }
-}
+// class Person {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
 
-class Students extends Person {
-  garde: string;
+//   constructor(name: string, age: number, hobbies: string[]) {
+//     this.name = name;
+//     this.age = age;
+//     this.hobbies = hobbies;
+//   }
+//   Test(): string {
+//     return `This is a name ${this.name} and THIS is a age ${
+//       this.age
+//     } and my hobbies is ${this.hobbies.join(",")} `;
+//   }
+// }
 
-  constructor(name: string, age: number, hobbies: string[], garde: string) {
-    super(name, age, hobbies);
-    this.garde = garde;
-  }
+// class Students extends Person {
+//   garde: string;
 
-  Test(): string {
-    return ` ${super.Test()} This  is a grade ${this.garde}`;
-  }
-}
+//   constructor(name: string, age: number, hobbies: string[], garde: string) {
+//     super(name, age, hobbies);
+//     this.garde = garde;
+//   }
 
-const perso1 = new Person("abdullah", 18, ["read", "book"]);
-const perso2 = new Students("abdullah", 18, ["read", "book"], "A");
+//   Test(): string {
+//     return ` ${super.Test()} and ${this.age} This  is a grade ${this.garde}`;
+//   }
+// }
 
-console.log(perso2.Test());
+// const perso1 = new Person("abdullah", 18, ["read", "book"]);
+// const perso2 = new Students("abdullah", 18, ["read", "book"], "A");
+
+// console.log(perso2.Test());
+// // console.log(perso1);
+/////////////[]
+
+// class Person {
+//   constructor(
+//     public name: string,
+//     public age: number,
+//     public hobbies: string[]
+//   ) {}
+
+//   introp(): string {
+//     return `My name is ${this.name} and my age is ${
+//       this.age
+//     } and my hobbies is ${this.hobbies.join(",")} `;
+//   }
+// }
+
+// class Student extends Person {
+//   constructor(
+//     public name: string,
+//     public age: number,
+//     public hobbies: string[],
+//     public grade: string
+//   ) {
+//     super(name, age, hobbies);
+//     this.grade = grade;
+//   }
+
+//   introc(): string {
+//     return `${super.introp()} and my garde is ${this.grade}`;
+//   }
+// }
+
+// const person12: Person = new Person("aa", 18, ["aa", "aa"]);
+// const Student1: Student = new Student(
+//   "abdullah",
+//   18,
+//   ["test", "codeing"],
+//   "ok"
+// );
+
+// // console.log(person12.introp());
+
+// console.log(Student1.introc());
+/////////////[]
+
+// class Person {
+//   private _age: number | undefined;
+//   constructor(public name: string, public hobbies: string[]) {}
+//   public set age(age: number) {
+//     if (age > 100 || age < 0) {
+//       throw new Error("This a is inviod");
+//     }
+//     this._age = age;
+//   }
+//   public get age(): number {
+//     if (this._age === undefined) {
+//       throw new Error("This a is undefined");
+//     } else {
+//       return this._age;
+//     }
+//   }
+//   introp(): string {
+//     return `My name is ${this.name} and my age is ${
+//       this._age
+//     }  and my hobbies is ${this.hobbies.join(",")} `;
+//   }
+// }
+
+// const person12: Person = new Person("abdullah", ["coding", "book"]);
+// person12.age = 71;
+// console.log(person12.introp());
+
+/////////////[]
+
+// class Test {
+//   public set age(age: number) {
+//     if (age > 510 || age < 0) {
+//       throw new Error("This a age inviold this catagre");
+//     } else {
+//       this._age12 = age;
+//     }
+//   }
+
+//   public get age(): number {
+//     if (this._age12 === undefined) {
+//       throw new Error("This a is undefined");
+//     } else {
+//       return this._age12;
+//     }
+//   }
+
+//   constructor(
+//     public name: string,
+//     private _age12: number | undefined,
+//     public hobbies: string[]
+//   ) {}
+
+//   intro(): string {
+//     return `My name is ${this.name} and my  ${
+//       this._age12
+//     } is  and my hobbies is ${this.hobbies.join(",")} `;
+//   }
+// }
+
+// const Data1: Test = new Test("abdullah", 18, ["codding", "Book"]);
+// Data1.age = 71;
+// console.log(Data1.intro());
+
+/////////////[]
+
+// class BankingAccount {
+//   private _Blance: number = 0;
+
+//   public get blance(): number {
+//     return this._Blance;
+//   }
+
+//   public set blance(newblance: number) {
+//     this._Blance = newblance;
+//     if (newblance < 0) {
+//       throw new Error("Your Account Blance is Zero");
+//     } else {
+//       this._Blance = newblance;
+//     }
+//   }
+// }
+
+// const acount=new BankingAccount()
+
+// acount.blance=10;
+
+// console.log(acount.blance);
+
+// class  Temperature{
+//   private _Celsius:number=0;
+
+//   public get Celsius():number{
+//      return this._Celsius;
+//   }
+
+//   public set Celsius(newCelsius:number){
+//       this._Celsius=newCelsius
+//   }
+
+//   public get Fahrenheit():number{
+// return (this._Celsius*9)/5*32
+//   }
+
+//   public set Fahrenheit(newFah:number){
+//     this._Celsius=((newFah-32)*5)/9
+//   }
+// }
+
+// const tem:any=new Temperature();
+
+// tem.Celsius=25;
+
+// console.log(tem.Fahrenheit);
+
+// class Test {
+//   private _Celsius: number = 0;
+
+//   public get Celsius(): number {
+//     return this._Celsius;
+//   }
+
+//   public set Celsius(newCelsius: number) {
+//     this._Celsius = newCelsius;
+//   }
+
+//   public get Fahrenheit(): number {
+//     return ((this._Celsius * 9) / 5) * 32;
+//   }
+
+//   public set Fahrenheit(newFah:number){
+//     this._Celsius=((newFah-32)*5)/9
+//   }
+
+// }
+// const temp=new Test();
+// temp.Celsius=25;
+// console.log(temp.Fahrenheit);
